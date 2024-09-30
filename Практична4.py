5c9693e4ee32a093bc00913ed9ced9850e006673
DeNic842gh <139693225+DeNic842gh@users.noreply.github.com>
2024-09-30 20:07:40 +0300

    Практична4.py

--- Практична4.py
+++ Практична4.py
@@ -1,13 +1,17 @@
-def sort(list):
-    element = list(set(input_list))
+def sort(inlist):
+    element = list(set(inlist))
+    return element
 
-    int_sort = sorted([x for x in element if isinstance(x, (int, float))])
-    str_sort = [x for x in element if not isinstance(x, (int, float))]
 
+def sort_int_str(inlist):
+    int_sort = sorted([x for x in inlist if isinstance(x, (int, float))])
+    str_sort = sorted([x for x in inlist if isinstance(x, str)])
     return int_sort + str_sort
 
 
-list = [1,2,3,4,5,6,3,4,5,7,6,5,4,3,4,5,4,3, 'Привіт', 'анаконда']
-result = sort(list)
+inlist = [1, 2, 3, 4, 5, 6, 3, 4, 5, 7, 6, 5, 4, 3, 4, 5, 4, 3, 'Привіт', 'анаконда']
+
+result = sort(inlist)
+result = sort_int_str(result)
 
 print(result)
